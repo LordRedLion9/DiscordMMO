a798b6cf3f2cd06486ebc62942da46b2ab3f4a94
LordRedLion9 <rockyslammer@gmail.com>
2017-07-25 18:28:25 +1200

    Setup Maven Dependancies

---
+++ DiscordMmoBot/src/mmoServer/ServerMain.java
@@ -0,0 +1,22 @@
+package mmoServer;
+
+import net.dv8tion.jda.core.*;
+
+public class ServerMain {
+
+	public static JDA jda;
+	
+	public static void main(String[] args) {
+		ServerMain s = new ServerMain();
+		s.run();
+	}
+	
+	public void run(){
+		
+		
+		
+		
+		
+	}
+
+}
